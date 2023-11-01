c2f4b6addc772c7444da8868fe2c4f03f441813f
rohith-zysk <rohith.badrinath@zysk.tech>
2023-11-02 00:17:55 +0530

    fix : home page

--- front-end/src/components/Home.tsx
+++ front-end/src/components/Home.tsx
@@ -22,13 +22,13 @@ export default function Example() {
             </p>
             <div className='mt-8 flex items-center justify-center gap-x-6'>
               <a
-                href='auth/register'
+                href='/vault/app/auth/register'
                 className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
               >
                 Get started
               </a>
               <a
-                href='auth/login'
+                href='/vault/app/auth/login'
                 className='rounded-md bg-indigo-600 px-7 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
               >
                 Login
