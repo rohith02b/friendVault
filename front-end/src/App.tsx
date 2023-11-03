671e52a00da084caa17e981c70dc582a137be738
rohith02b <rohith20b@gmail.com>
2023-11-03 22:55:23 +0530

    netlify setup

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './components/Home'
-import Login from './components/Auth/Login'
-import Register from './components/Auth/Register'
-import axios from 'axios'
-import Redirector from './components/Redirector'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import axios from 'axios';
+import Redirector from './components/Redirector';
 
 function App() {
-  axios.defaults.withCredentials = true
-  const baseUrl = import.meta.env.VITE_BASE_ROUTE
-  const authUrl = import.meta.env.VITE_AUTH_SERVICE_URL
+  axios.defaults.withCredentials = true;
+  const baseUrl = import.meta.env.VITE_BASE_ROUTE;
+  const authUrl = import.meta.env.VITE_AUTH_SERVICE_URL;
 
-  console.log(baseUrl, authUrl)
+  console.log(baseUrl, authUrl);
 
   return (
     <>
@@ -22,7 +22,7 @@ function App() {
         <Route path={`${baseUrl}auth/login`} element={<Login />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
