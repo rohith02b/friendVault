590d4ca6abc9c79301fe6d55e2c97b1f87eec969
rohith02b <rohith20b@gmail.com>
2023-11-03 23:25:41 +0530

    fix : server config

--- back-end/server.js
+++ back-end/server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const authRoutes = require('./src/routes/Auth')
-dotenv.config()
-const PORT = process.env.PORT
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const authRoutes = require('./src/routes/Auth');
+dotenv.config();
+const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
+app.get('/vault/service/', (req, res) => {
   return res.json({
     message: PORT,
-  })
-})
+  });
+});
 
 app.use(
   cors({
@@ -22,13 +22,13 @@ app.use(
     allowedHeaders: ['Content-Type', 'Authorization'],
     origin: process.env.PUBLIC_URL,
   })
-)
+);
 
 //Auth
-app.use('/vault/service/api/auth', authRoutes)
+app.use('/vault/service/api/auth', authRoutes);
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!")
-})
+  res.status(404).send("Sorry can't find that!");
+});
 app.listen(PORT, () => {
-  console.log('Working')
-})
+  console.log('Working');
+});
