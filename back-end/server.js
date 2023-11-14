cc39f9d38abe844e509df27a4c300e8969abb32b
rohith02b <rohith20b@gmail.com>
2023-11-15 00:51:57 +0530

    feat : env test

--- back-end/server.js
+++ back-end/server.js
@@ -34,4 +34,9 @@ app.use((req, res, next) => {
 });
 app.listen(PORT, () => {
   console.log('Working');
+  try {
+    console.log(process.env.DEMO);
+  } catch (error) {
+    console.log('Env did not work');
+  }
 });
