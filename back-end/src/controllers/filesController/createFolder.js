c82a3d18f246bd218e0516bc4421d6bf089e28e4
rohith02b <rohith20b@gmail.com>
2023-11-15 00:24:38 +0530

    fix : errors

--- back-end/src/controllers/filesController/createFolder.js
+++ back-end/src/controllers/filesController/createFolder.js
@@ -1,13 +1,5 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
-
-const connectionString = process.env.CONNECTION_STRING;
-const blobServiceClient =
-  BlobServiceClient.fromConnectionString(connectionString);
-const container = process.env.CONTAINER;
-const containerClient = blobServiceClient.getContainerClient(container);
-
 const createFolder = async (req, res) => {
-  const groupId = req.params.groupId;
+  return res.json('hello');
 };
 
 module.exports = { createFolder };
--- back-end/src/controllers/filesController/uploadFile.js
+++ back-end/src/controllers/filesController/uploadFile.js
@@ -1,47 +1,47 @@
-const { BlobServiceClient } = require('@azure/storage-blob');
+// const { BlobServiceClient } = require('@azure/storage-blob');
 const fs = require('fs');
 const uniqueId = require('uniqid');
 const prisma = require('../../dbConnect/connection');
 
-const connectionString = process.env.CONNECTION_STRING;
-const blobServiceClient =
-  BlobServiceClient.fromConnectionString(connectionString);
-const container = process.env.CONTAINER;
-const containerClient = blobServiceClient.getContainerClient(container);
+// const connectionString = process.env.CONNECTION_STRING;
+// const blobServiceClient =
+//   BlobServiceClient.fromConnectionString(connectionString);
+// const container = process.env.CONTAINER;
+// const containerClient = blobServiceClient.getContainerClient(container);
 
 const uploadFile = async (req, res) => {
   try {
-    const files = req.files;
-    const group = req.params.groupId;
-    files.map(async (file) => {
-      const id = uniqueId.time();
-      const fileProps = file.originalname.split('.');
-      const fileName = `${fileProps[0]}-${id}.${
-        fileProps[fileProps.length - 1]
-      }`;
+    // const files = req.files;
+    // const group = req.params.groupId;
+    // files.map(async (file) => {
+    //   const id = uniqueId.time();
+    //   const fileProps = file.originalname.split('.');
+    //   const fileName = `${fileProps[0]}-${id}.${
+    //     fileProps[fileProps.length - 1]
+    //   }`;
 
-      let path = '';
-      if (req.query.path) {
-        path = `${group}${req.query.path}/${fileName}`;
-      } else {
-        path = `${group}/${fileName}`;
-      }
+    //   let path = '';
+    //   if (req.query.path) {
+    //     path = `${group}${req.query.path}/${fileName}`;
+    //   } else {
+    //     path = `${group}/${fileName}`;
+    //   }
 
-      console.log;
-      const blockBlobClient = containerClient.getBlockBlobClient(path);
+    //   console.log;
+    //   const blockBlobClient = containerClient.getBlockBlobClient(path);
 
-      await blockBlobClient.uploadFile(file.path);
-      await prisma.files.create({
-        data: {
-          group_id: group,
-          url: blockBlobClient.url,
-          path: req.query.path || '/',
-          file_name: file.originalname,
-          type: 'file',
-        },
-      });
-      fs.unlinkSync(file.path);
-    });
+    //   await blockBlobClient.uploadFile(file.path);
+    //   await prisma.files.create({
+    //     data: {
+    //       group_id: group,
+    //       url: blockBlobClient.url,
+    //       path: req.query.path || '/',
+    //       file_name: file.originalname,
+    //       type: 'file',
+    //     },
+    //   });
+    //   fs.unlinkSync(file.path);
+    // });
     return res.json('Uploaded successfully');
   } catch (error) {
     return res.json('File error');
