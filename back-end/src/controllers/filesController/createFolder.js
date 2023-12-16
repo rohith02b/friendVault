182742227176beaecc16358ee53f65ac4d1b9d1f
rohith02b <rohith20b@gmail.com>
2023-12-16 18:40:57 +0530

    fix : create folder

--- back-end/src/controllers/filesController/createFolder.js
+++ back-end/src/controllers/filesController/createFolder.js
@@ -1,26 +1,26 @@
-const prisma = require('../../dbConnect/connection')
-const uniqid = require('uniqid')
-
+const prisma = require('../../dbConnect/connection');
+const uniqid = require('uniqid');
 
 const createFolder = async (req, res) => {
   const id = uniqid.time();
-  const {name} = req.body;
-  const {path} = req.query;
-  const {groupId} = req.params
+  const { name } = req.body;
+  const { path } = req.query;
+  const { groupId } = req.params;
   try {
     await prisma.content.create({
-        data: {
-          content_id: id,
-          group_id: groupId,
-          url: name,
-          path: path || '/',
-          content_name: name,
-          content_type: 'folder',
-        },
-      });
-      res.json("Created successfully")
+      data: {
+        content_id: id,
+        group_id: groupId,
+        url: name,
+        path: path || '/',
+        content_name: name,
+        content_type: 'folder',
+        uploaded: true,
+      },
+    });
+    res.json('Created successfully');
   } catch (error) {
-    res.json("Error")
+    res.json('Error');
   }
 };
 
