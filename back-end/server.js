f4a9d03c9b20d829946b14fc7050a0e0228c7502
rohith02b <rohith20b@gmail.com>
2023-12-17 20:34:47 +0530

    fix : path testing

--- back-end/server.js
+++ back-end/server.js
@@ -44,4 +44,6 @@ app.use((req, res, next) => {
 });
 app.listen(PORT, () => {
   console.log('Working');
+  const serverFolderPath = process.cwd();
+  console.log('Server folder path:', serverFolderPath);
 });
